3872f6aa7d4a2cb4238b86c8559db145d05bb75d
Rafael Cavalcante chaves <rafael.chaves@polodeinovacao.ifpb.edu.br>
2023-03-26 12:11:15 -0300

    Adionando Compose files

--- server/app.py
+++ server/app.py
@@ -26,4 +26,4 @@ def get_post():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host='0.0.0.0', port=5000)
